0685624f3f8581f064d9a7621e45222f30d56a0d
guddu-debasis <debasis2122004@gmail.com>
2024-06-15 10:22:13 +0530

    add

---
+++ alert_prompt.js
@@ -0,0 +1,39 @@
+// let num=prompt(" enter a number:");
+// if(num % 5 === 0){
+//     console.log(num,"multiple of 5");
+// }
+// else{
+//     console.log(num," not a multiple of 5");
+// }
+let p=alert("HEY I WANT TO SAY YOU SOMETHING!");
+let Q=alert("ARE YOU READY?");
+let x=alert("DONT GET BORED");
+let y=alert("TU PAGAL AUART HAI");
+// let z=alert("AGAR NHI KARTI HAI TOH KYU NHI Karti hai,kya kami hai tujhme");
+
+
+// let score= prompt("enter the score:");
+// let grade;
+
+// if(score>=90 && score<=100){
+//     grade= "A";
+// }
+// else if(score>=80 && score<90){
+//     grade= "B";
+// }
+// else if(score>=70 && score<80){
+//     grade= "C";
+// }
+// else if(score>=60 && score<70){
+//     grade= "D";
+// }
+// else if(score>=50 && score<60){
+//     grade= "E";
+// }
+// else if(score>=50 && score<60){
+//     grade="F";
+// }
+// else{
+//     grade="FAIL";
+// }
+// console.log("According to score your grade is:",grade);
